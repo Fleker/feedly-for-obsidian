f5eaadc52b133d1079de7dc27278a01de64df2ea
Nick Felker <handnf@gmail.com>
2025-01-09 20:45:03 -0500

    Initial working version

---
+++ README.md
@@ -0,0 +1,14 @@
+# Trakt for Obsidian
+
+This project syncs your Trakt checkins to a file in Obsidian with date-specific backlinks. This can help you keep track of movies you've seen as well as when you've seen them.
+
+## Example
+
+You open up the command palette and run the **Trakt Sync: Sync watched history** command. It then fetches data and places it in a file called `Trakt Rating History.md` in a bulleted list.
+
+```md
+Gave 8/10 to Season 2, Episode 1 of [Abbott Elementary](https://trakt.tv/shows/abbott-elementary): ["Development Day"](https://trakt.tv/shows/abbott-elementary/seasons/2/episodes/1) on [[2024-10-22]]
+Finished watching Season 1, Episode 2 of [The Secret Lives of Mormon Wives](https://trakt.tv/shows/the-secret-lives-of-mormon-wives) on [[2024-09-22]]
+```
+
+This plugin could use your feedback and help to make it a success!
---
+++ main.ts
@@ -0,0 +1,255 @@
+import { App, Notice, Plugin, PluginSettingTab, Setting, normalizePath, requestUrl } from 'obsidian';
+
+interface FeedlySettings {
+	userId?: string
+	accessToken?: string
+	/** Timestamp of last sync. Used for querying. */
+	lastSync?: number
+	/** Save this token for continue sync if it was disrupted for some reason (API rate limit) */
+	continuationToken?: string
+	/** Path for folder to save all these files */
+	annotationsFolder?: string
+}
+
+interface FeedlyAnnotatedEntry {
+	annotation: {
+		highlight?: {
+			text: string
+		}
+		comment?: string
+	}
+	entry: {
+		canonicalUrl: string
+		id: string
+		published?: number
+		crawled: number
+		author: string
+		title: string
+		origin?: {
+			title: string
+		}
+	}
+	created: number
+}
+
+const DEFAULT_SETTINGS: FeedlySettings = {
+	annotationsFolder: 'Feedly Annotations'
+}
+
+const apiCall = async (accessToken: string, path: string, method = 'GET', data?: any) => {
+ 	console.debug(`https://cloud.feedly.com/v3/${path}`)
+	try {
+		const res = await requestUrl({
+			url: `https://cloud.feedly.com/v3/${path}`,
+			method,
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+				Accept: 'application/json',
+			},
+			body: data ?? undefined
+		})
+  	return res.json
+	} catch (e) {
+		console.debug(e)
+		throw new Error(e)
+	}
+}
+
+function sanitizeFrontmatter(v: string = '') {
+  return v.replace(/:/g, ' - ')
+}
+
+function dateToJournal(date: Date) {
+	return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+}
+
+function getInitialContent(entry: FeedlyAnnotatedEntry) {
+  return `---${entry.entry.canonicalUrl ? `
+url: ${entry.entry.canonicalUrl}` : ''}
+feedlyUrl: https://feedly.com/i/entry/${entry.entry.id}
+date: ${dateToJournal(new Date(entry.created))}
+pubDate: ${dateToJournal(new Date(entry.entry.published ?? entry.entry.crawled))}
+author: ${sanitizeFrontmatter(entry.entry.author)}${entry.entry.origin?.title ? `
+publisher: ${sanitizeFrontmatter(entry.entry.origin.title)}` : ''}
+---
+`
+}
+
+function getAppendContent(entry: FeedlyAnnotatedEntry) {
+  if (entry.annotation.highlight) {
+    return `
+
+> ${entry.annotation.highlight.text.replace(/\n/g, `
+>
+> `)}`
+  } else if (entry.annotation.comment) {
+    return `
+    
+${entry.annotation.comment}`
+  }
+  console.warn('No append content for', entry.entry.title)
+  return ''
+}
+
+async function getAnnotations(accessToken: string, continuation?: string, syncTime: number = 0) {
+  try {
+		const a = await apiCall(accessToken, `annotations/journal?newerThan=${syncTime}&withEntries=true&count=100${continuation ? `&continuation=${continuation}` : ''}`)
+		if (a.errorCode) {
+			throw new Error(a)
+		}
+		return {
+			entries: a.entries,
+			continuation: a.continuation,
+			count: a.entries.length
+		}
+	} catch (e) {
+		console.error(e)
+		if (e.message.includes('status 401')) {
+			console.error('Access token expired', e)
+			console.log(continuation)
+			throw new Error('Access token expired, request a new one')
+		}
+		if (e.message.includes('status 429')) {
+			console.error('API rate limit reached', e)
+			console.log(continuation)
+			throw new Error('API rate limit reached')
+		}
+	}
+}
+
+export default class FeedlyPlugin extends Plugin {
+	settings: FeedlySettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		this.addCommand({
+			id: 'sync',
+			name: 'Sync Annotated Articles',
+			callback: async () => {
+				await this.loadSettings()
+				if (!this.settings.userId) {
+					return new Notice('Missing Feedly user id')
+				}
+				if (!this.settings.accessToken) {
+					return new Notice('Missing Feedly access token')
+				}
+
+				let entryCounter = 0
+				let continuationToken = this.settings.continuationToken ?? undefined
+				const syncStartTime = Date.now()
+				new Notice('Beginning to download Feedly annotations')
+
+				const folderName = this.settings.annotationsFolder ?? 'Feedly Annotations'
+				const doesFolderExist = this.app.vault.getFolderByPath(folderName)
+				if (!doesFolderExist) {
+					await this.app.vault.createFolder(folderName)
+				}
+				const processAnnotations = async (entries: FeedlyAnnotatedEntry[]) => {
+					for (const e of entries) {
+						// Remove slashes.
+						// File name cannot contain any of the following characters: * " \ / < > : | ?
+						const sanitizedFileName = e.entry.title.replace(/[*"\/<>:|?]/g, '')
+						const filename = `${sanitizedFileName}.md`
+						const path = normalizePath(`${folderName}/${filename}`)
+						let obsidianFile = this.app.vault.getFileByPath(path)
+						if (!obsidianFile) {
+							// Add the frontmatter
+							obsidianFile = await this.app.vault.create(path, getInitialContent(e))
+						}
+						// Add the highlight or comment of this entry
+						await this.app.vault.append(obsidianFile!, getAppendContent(e))
+						entryCounter++
+					}
+				}
+
+				while (true) {
+					try {
+						const res = await getAnnotations(this.settings.accessToken, continuationToken, this.settings.lastSync)
+						if (!res?.continuation) break // Exit out
+						continuationToken = res.continuation
+						await processAnnotations(res.entries)
+						if (res.count < 100) {
+							console.debug(`only got ${res.count} entries`)
+							this.settings.continuationToken = continuationToken
+							this.settings.lastSync = syncStartTime
+							this.saveSettings(this.settings)
+							new Notice('All Feedly annotations synced')
+							break
+						}
+						this.settings.continuationToken = continuationToken
+						// Save token for the future
+						this.saveSettings(this.settings)
+						console.debug(`>    at ${continuationToken} ...`)
+					} catch (e) {
+						if (e.message.includes('API rate limit')) {
+							new Notice('The Feedly API rate limit has been reached, continue later')
+						} else {
+							new Notice(e.message)
+							console.error(e)
+						}
+						break;
+					}
+				}
+				new Notice(`Synced ${entryCounter} annotations`)
+			},
+		})
+
+		this.addSettingTab(new FeedlySettingTab(this.app, this));
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings(settings: FeedlySettings) {
+		await this.saveData(settings);
+	}
+}
+
+class FeedlySettingTab extends PluginSettingTab {
+	plugin: FeedlyPlugin;
+	settings: FeedlySettings;
+
+	constructor(app: App, plugin: FeedlyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	async display(): Promise<void> {
+		const {containerEl} = this;
+		this.settings = await this.plugin.loadData() ?? DEFAULT_SETTINGS
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Feedly User ID')
+			.addText((component) => {
+				component.setValue(this.settings.userId ?? '')
+				component.onChange(async (value) => {
+					this.settings.userId = value
+					await this.plugin.saveSettings(this.settings)
+				})
+			})
+
+		new Setting(containerEl)
+			.setName('Access Token')
+			.addText((component) => {
+				component.setValue(this.settings.accessToken ?? '')
+				component.onChange(async (value) => {
+					this.settings.accessToken = value
+					await this.plugin.saveSettings(this.settings)
+				})
+			})
+
+		new Setting(containerEl)
+			.setName('Sync Directory')
+			.addText((component) => {
+				component.setValue(this.settings.annotationsFolder ?? '')
+				component.onChange(async (value) => {
+					this.settings.annotationsFolder = value
+					await this.plugin.saveSettings(this.settings)
+				})
+			})
+	}
+}
